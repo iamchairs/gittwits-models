3f5074e284ff6f40a66b35acd571b3d83c726b6d
Micah Williamson <micahwllmsn@gmail.com>
2015-07-23 18:56:33 -0500

    shouldve tested it first

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-model.exports = {
+module.exports = {
   Twit: require('./models/twit.model'),
-  Hashtag: require('./models/hashtag.model')
+  Hashtag: require('./models/hashtag.model'),
   User: require('./models/user.model')
 };
